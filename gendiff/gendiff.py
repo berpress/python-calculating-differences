fdf4befee34975d4168997f7114b628e6e61133f
Alexander <berpress@gmail.com>
2019-12-21 15:12:20 +0300

    Add --help for gendiff

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -2,7 +2,11 @@ import argparse
 
 
 def main():
-    print("test")
+    parser = argparse.ArgumentParser(description='Generate diff')
+    parser.add_argument('first_file', type=str)
+    parser.add_argument('second_file', type=str)
+    args = parser.parse_args()
+    print(args.indir)
 
 
 if __name__ == '__main__':
