eff29a4422bc1e608dadc54926cfcccb586f130f
Alexander <berpress@gmail.com>
2019-12-22 09:34:02 +0300

    Add format option

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -2,11 +2,12 @@ import argparse
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Generate diff')
+    parser = argparse.ArgumentParser(description='Generate diff.')
     parser.add_argument('first_file', type=str)
     parser.add_argument('second_file', type=str)
+    parser.add_argument('-f', '--format', help='set format of output')
     args = parser.parse_args()
-    print(args.indir)
+    print(args.first_file)
 
 
 if __name__ == '__main__':
