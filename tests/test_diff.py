8a302e225b5b8cf4b2eff465d2957f516f46e01f
berpress <berpress@gmail.com>
2020-01-28 21:59:34 +0400

    Add tests

--- tests/test_diff.py
+++ tests/test_diff.py
@@ -4,7 +4,7 @@ from gendiff.gendiff import generate_diff
 import os
 
 TEST_FILES_JSON = 'tests/fixtures/json/json_file/'
-TEST_FILES_YAML = 'tests/fixtures/json/yaml_file/'
+TEST_FILES_YAML = 'tests/fixtures/yaml/yaml_file/'
 
 
 def test_flat_json_file():
@@ -25,12 +25,6 @@ def test_text_json_file():
     assert data_file == get_plain_diff(diff)
 
 
-# def test_float_yaml_file():
-#     first_file = 'tests/fixtures/yaml/after.yml'
-#     second_file = 'tests/fixtures/yaml/before.yml'
-#     assert FLOAT_RES == generate_diff(first_file, second_file)
-
-
 def test_nested_flat_json_file():
     first_file = os.path.join(TEST_FILES_JSON, "h_first.json")
     second_file = os.path.join(TEST_FILES_JSON, "h_second.json")
@@ -48,7 +42,39 @@ def test_nested_text_json_file():
     data_file = f.read()
     assert data_file == get_plain_diff(diff)
 
-# def test_not_float_yaml_file():
-#     first_file = 'tests/fixtures/yaml/before_nested.yml'
-#     second_file = 'tests/fixtures/yaml/after_nested.yml'
-#     assert NOT_FLOAT_YAML == generate_diff(first_file, second_file)
+
+def test_flat_yaml_file():
+    first_file = os.path.join(TEST_FILES_YAML, "after.yml")
+    second_file = os.path.join(TEST_FILES_YAML, "before.yml")
+    diff = generate_diff(first_file, second_file)
+    f = open('tests/fixtures/yaml/yaml_result/flat_diff.txt')
+    data_file = f.read()
+    assert data_file == get_dict_diff(diff)
+
+
+def test_nested_flat_yaml_file():
+    first_file = os.path.join(TEST_FILES_YAML, "after_nested.yml")
+    second_file = os.path.join(TEST_FILES_YAML, "before_nested.yml")
+    diff = generate_diff(first_file, second_file)
+    f = open('tests/fixtures/yaml/yaml_result/flat_nested_diff.txt')
+    data_file = f.read()
+    assert data_file == get_dict_diff(diff)
+
+
+def test_text_yaml_file():
+    first_file = os.path.join(TEST_FILES_YAML, "after.yml")
+    second_file = os.path.join(TEST_FILES_YAML, "before.yml")
+    diff = generate_diff(first_file, second_file)
+    f = open('tests/fixtures/yaml/yaml_result/flat_text_diff.txt')
+    data_file = f.read()
+    assert data_file == get_plain_diff(diff)
+
+
+def test_nested_text_yaml_file():
+    first_file = os.path.join(TEST_FILES_YAML, "after_nested.yml")
+    second_file = os.path.join(TEST_FILES_YAML, "before_nested.yml")
+    diff = generate_diff(first_file, second_file)
+    f = open('tests/fixtures/yaml/yaml_result/flat_text_nested_diff.txt')
+    data_file = f.read()
+    assert data_file == get_plain_diff(diff)
+
